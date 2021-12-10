b67e3b5db4d6c9ef6a01d89b965b447a8c59abd1
Boglarka_Szalay <bogge88@gmail.com>
2021-12-10 16:27:09 +0100

    codewar session

--- middle.py
+++ middle.py
@@ -1,20 +1,8 @@
-def spin_words(sentence):
-    words = sentence.split(' ')
-    result = ''
-    for word in words:
-        if len(word) >= 5:
-            word = word[::-1]
-            result = f'{result} {word}'
-        else:
-            result = f'{result} {word}'
-            
-    return result[1:]
-
-print(spin_words('hey coder warrior'))
-
-
-
-def spin_words2(sentence):
-    words = [word for word in sentence.split(" ")]
-    words = [word if len(word) < 5 else word[::-1] for word in words]
-    return " ".join(words)
+def get_middle(s):
+    if len(s)%2 == 0:
+        middle = int(len(s) / 2)
+        result = s[middle-1:middle+1]
+    else:
+        middle = int(len(s) // 2)
+        result = s[middle]
+    return result
--- tasks.py
+++ tasks.py
@@ -1,32 +1,53 @@
-# def opposite(number):
-#     return -number
+def opposite(number):
+    return -number
 
-# print(opposite(777))
+print(opposite(777))
 
 
 # ##########################
 
-# def positive_sum(arr):
-#     positive_arr = []
-#     for a in arr:
-#         if a > 0:
-#             positive_arr.append(a)
-#     if len(positive_arr) == 0:
-#         return 0
-#     return sum(positive_arr)
+def positive_sum(arr):
+    positive_arr = []
+    for a in arr:
+        if a > 0:
+            positive_arr.append(a)
+    if len(positive_arr) == 0:
+        return 0
+    return sum(positive_arr)
 
-# print(positive_sum([-1,2,3,4,-5]))
+print(positive_sum([-1,2,3,4,-5]))
 
 ############################
 
-
 def how_much_i_love_you(nb_petals):
     how_much = ["I love you","a little","a lot","passionately","madly","not at all"]
-
-    for p in range(nb_petals):
-       print(how_much[nb_petals])
+    num = (nb_petals % 6)-1
+    return how_much[num]
         
 
-how_much_i_love_you(20)
-        
+print(how_much_i_love_you(15))
 
+#################################################
+
+def spin_words(sentence):
+    words = sentence.split(' ')
+    result = ''
+    for word in words:
+        if len(word) >= 5:
+            word = word[::-1]
+            result = f'{result} {word}'
+        else:
+            result = f'{result} {word}'
+            
+    return result[1:]
+
+
+##############################################
+#    16  -->  1 + 6 = 7
+#    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+# 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+
+def digital_root(n):
+    while n > 9:
+        n = sum(map(int, str(n)))
+    return n
